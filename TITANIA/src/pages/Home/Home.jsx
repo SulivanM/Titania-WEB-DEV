476139c40e7a64d255ac4219d100b6e56b323391
pclmentc <pclmentc@gmail.com>
2024-07-04 15:58:07 +0200

    carrousel

--- TITANIA/src/pages/Home/Home.jsx
+++ TITANIA/src/pages/Home/Home.jsx
@@ -1,10 +1,31 @@
 import './Home.scss';
+import Ark from '../../assets/titania_ark.jpg';
+import Bob from '../../assets/titania_BOB.jpg';
+import Dod from '../../assets/titania_DOD.jpg';
+import Farm from '../../assets/titania_farming.jpg';
+import Gmod from '../../assets/Titania_GMOD_V2.jpg';
+import Path from '../../assets/titania_path.jpg';
+import Isle from '../../assets/titania_the_isle.jpg';
+
 
 const Home = () => {
   return (
     <div className='main'>
-      <h1>Nos Jeux & Serveurs De Jeux</h1>
+      <h1>Nos Jeux & Serveurs</h1>
       <p>Titania est partout ! Ordinateur, tablette, console ou téléphone.</p>
+      <section id="slideshow">
+        <div className="entire-content">
+          <div className="content-carrousel">
+            <figure className="shadow"><img src={Ark} /></figure>
+            <figure className="shadow"><img src={Bob} /></figure>
+            <figure className="shadow"><img src={Dod} /></figure>
+            <figure className="shadow"><img src={Farm} /></figure>
+            <figure className="shadow"><img src={Gmod} /></figure>
+            <figure className="shadow"><img src={Path} /></figure>
+            <figure className="shadow"><img src={Isle} /></figure>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
