ff749af9f990c2b0f92c2c02bd66cd10bffd6b40
pclmentc <pclmentc@gmail.com>
2024-08-02 20:32:09 +0200

    feat link games

--- TITANIA/src/data/gamesData.jsx
+++ TITANIA/src/data/gamesData.jsx
@@ -6,6 +6,7 @@ const gamesData = [
     description: "Les joueurs doivent survivre dans un monde rempli de dinosaures, d'animaux préhistoriques et toutes sortes de créatures disparues, de dangers naturels et potentiellement de joueurs humains hostiles..",
     image: "/images/Ark.webp",
     category: "Survival",
+    link: "https://arkascended.fr/"
   },
   {
     id: 2,
@@ -13,6 +14,7 @@ const gamesData = [
     description: "Le joueur évolue en tant que dinosaure, ou autre créature préhistorique. Le joueur fait partie d'un écosystème vivant, créé par les joueurs. L'ultime objectif est de survivre sur une des îles de Beasts of Bermuda.",
     image: "/images/Bob.webp",
     category: "Survival",
+    link: "https://beastsofbermuda.com/"
   },
   {
     id: 3,
@@ -20,6 +22,7 @@ const gamesData = [
     description: "Un jeu de survie se déroulant dans un vaste et magnifique monde ouvert avec plusieurs biomes et créatures distinctes. ",
     image: "/images/Dod.webp",
     category: "Survival",
+    link: "https://dayofdragons.com",    
   },
   {
     id:4,
@@ -27,6 +30,7 @@ const gamesData = [
     description: "Venez vivre votre propre aventure dans l'univers de GTA.",
     image: "/images/GTA-RP.webp",
     category: "Battle",
+    link: "https://www.rockstargames.com/fr/gta-v"
   },
   {
     id:5,
@@ -34,6 +38,7 @@ const gamesData = [
     description: "Survivrez-vous longtemps dans ce monde post-apocalyptique ? Un monde envahi de zombies, où il faudra partager de maigres ressources avec les autres survivants.",
     image: "/images/dayZ.webp",
     category: "Battle",
+    link: "https://fr.idcgames.com/dayz/"
   },
   {
     id:6,
@@ -41,6 +46,7 @@ const gamesData = [
     description: "Fais grandir ta bête de l'état de nouveau-né à l'âge adulte, acquérant de nouvelles attaques et personnalisant ton apparence au fur et à mesure que tu complètes des quêtes.",
     image: "/images/pot.webp",
     category: "Survival",
+    link: "https://pathoftitans.com/"
   },
   {
     id:7,
@@ -48,6 +54,7 @@ const gamesData = [
     description: "Réveille le vampire qui est en toi. Chasse le sang dans les colonies voisines pour retrouver tes forces et fuis le soleil brûlant pour survivre.",
     image: "/images/V-R.webp",
     category: "Battle",
+    link: "https://vrising-france.com/"
   },
   // Ajoutez d'autres jeux ici
 ];
